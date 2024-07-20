83e93bcdab622d400e2dc7166826dd4891477f1a
AyeshaSaria014 <ayeshasaria14@gmail.com>
2024-07-21 00:22:38 +0530

    my work

---
+++ src/components/common/header/Head.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container flexSB paddingTB'>
+          <div className='logo'>
+             <img src='logo image.png' alt='news' />
+            </div>
+          <div className='ad'>
+            <img src="ad image.jpg" alt='ad' />
+            </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
